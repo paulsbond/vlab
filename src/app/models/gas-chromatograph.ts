a50cd8c168f9f942647f1585a8fb881e988558c9
Paul Bond <git@paulsbond.co.uk>
2020-05-18 23:03:14 +0100

    Gaussian class to calculate width

--- src/app/models/gas-chromatograph.ts
+++ src/app/models/gas-chromatograph.ts
@@ -1,12 +1,11 @@
-import { Sample } from './sample';
 import { Chart } from './chart';
+import { Gaussian } from './gaussian';
+import { Sample } from './sample';
 
 export class GasChromatograph {
   private _max_voltage: number = 1000;
   private _runtime: number = 180;
-  private _time: number;
   private _running: boolean = false;
-  private _interval;
 
   public chart: Chart = new Chart('Time / s', 'Voltage / mV', 180, 1000);
   public result: Result;
@@ -19,27 +18,27 @@ export class GasChromatograph {
 
   inject(sample: Sample): void {
     if (this._running) return;
-    const a = (sample.percentage * 1000) / 5.2; // Peak height
-    const b = 120; // Peak center
-    const c = 20; // Standard deviation
+    const height = (sample.percentage * 1000) / 5.2;
+    const cut_height = Math.min(height, this._max_voltage);
+    const position = 120;
+    const sd = 20;
+    const gaussian = new Gaussian(height, position, sd);
     let area = 0;
-    this._time = 0;
+    let time = 0;
     this._running = true;
-    this._interval = setInterval(() => {
-      let voltage =
-        a * Math.exp(-Math.pow(this._time - b, 2) / (2 * Math.pow(c, 2)));
-      if (voltage > this._max_voltage) voltage = this._max_voltage;
+    const interval = setInterval(() => {
+      const voltage = Math.min(gaussian.y(time), this._max_voltage);
       area += voltage;
-      this.chart.add_point(this._time, voltage);
-      this._time++;
-      if (this._time > this._runtime) {
+      this.chart.add_point(time, voltage);
+      time++;
+      if (time > this._runtime) {
         this._running = false;
-        clearInterval(this._interval);
+        clearInterval(interval);
         this.result = {
-          retention_time: b,
+          retention_time: position,
           area: area,
-          height: a > this._max_voltage ? this._max_voltage : a,
-          fwhm: 2.355 * c,
+          height: cut_height,
+          fwhm: gaussian.width(cut_height / 2),
         };
       }
     }, 10);
