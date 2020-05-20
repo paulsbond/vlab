0e6fc8d1b375da8e5185c9610597eee7c57c99be
Paul Bond <git@paulsbond.co.uk>
2020-05-21 00:11:28 +0100

    Change to form validation

--- src/app/dilution/dilution.component.ts
+++ src/app/dilution/dilution.component.ts
@@ -1,5 +1,7 @@
-import { Component, OnInit } from '@angular/core';
+import { Component, OnInit, ViewChild } from '@angular/core';
+import { NgForm } from '@angular/forms';
 import { StateService } from '../state.service';
+import { decimal_places } from '../models/utils';
 
 @Component({
   selector: 'app-dilution',
@@ -7,13 +9,31 @@ import { StateService } from '../state.service';
   styleUrls: ['./dilution.component.scss'],
 })
 export class DilutionComponent implements OnInit {
-  constructor(public state: StateService) {}
-  ngOnInit(): void {}
+  @ViewChild('dilutionForm', { static: true }) dilutionForm: NgForm;
+  formChangesSubscription: any;
+  error: string;
 
-  volume_change(): void {
-    console.log(this.state.dilution.flask);
-    this.state.dilution.volume =
-      Math.round((this.state.dilution.volume + Number.EPSILON) * 10) / 10;
+  constructor(public state: StateService) {}
+  ngOnInit(): void {
+    this.formChangesSubscription = this.dilutionForm.valueChanges.subscribe(
+      (values) => {
+        if (!values.label) {
+          this.error = 'Label is required';
+        } else if (!values.volume) {
+          this.error = 'Sample volume is required';
+        } else if (values.volume > values.flask.volume) {
+          this.error = 'Sample will not fit in the flask';
+        } else if (decimal_places(values.volume) > 1) {
+          this.error = 'Can only measure sample to 1 d.p.';
+        } else {
+          this.error = '';
+        }
+      }
+    );
+  }
+
+  ngOnDestroy() {
+    this.formChangesSubscription.unsubscribe();
   }
 
   dilute(): void {
